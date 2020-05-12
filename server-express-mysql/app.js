e3b2badd9463e49ab6d466b092a709c5c04bd3c1
Jeff C <coen.jeffery@outlook.com>
2020-05-12 15:49:37 -0700

    fixed tasks 404 error

--- server-express-mysql/app.js
+++ server-express-mysql/app.js
@@ -8,12 +8,21 @@ var models = require('./models');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var tasksRouter = require('./routes/tasks');
+
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
+
+//Jeff: added CORS code
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -23,6 +32,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/tasks', tasksRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
