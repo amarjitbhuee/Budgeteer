7d161a2e8c5bd47a300575e99604bbcd4e2403a8
AJ Bhuee <39472015+amarjitbhuee@users.noreply.github.com>
2020-05-25 23:52:44 -0700

    adjusted the App.js to view Transactions

--- client-react/src/App.js
+++ client-react/src/App.js
@@ -1,21 +1,21 @@
 import React from "react";
-// import Transaction from './components/Transaction';
-import User from './components/User';
 import "./App.css";
 //Jeff imported Update from screens
 import History from "./screens/History";
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
-// import Edit from "./screens/Edit";
-import EditUser from "./screens/EditUser";  
+import Edit from "./screens/Edit";
+// import User from './components/User';
+// import EditUser from "./screens/EditUser";  
+import Transaction from "./components/Transaction";
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Switch>
-          <Route path="/" exact component={ User } />
+          <Route path="/" exact component={ Transaction } />
           <Route path="/history"  component={ History } />
-          <Route path="/edituser/:userid" component={ EditUser } />
+          <Route path="/edituser/:userid" component={ Edit } />
         </Switch>
       </div>
     </Router>
