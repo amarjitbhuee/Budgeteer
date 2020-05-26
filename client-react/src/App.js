27b5cdc432b72468cc63e8b6dd6777c80542a745
Jeff C <coen.jeffery@outlook.com>
2020-05-26 07:39:30 -0700

    Added Title, Favicon and started layout - Verified with Josh

--- client-react/src/App.js
+++ client-react/src/App.js
@@ -2,15 +2,26 @@ import React from "react";
 import Transaction from './components/Transaction';
 // import User from './components/User';
 import "./App.css";
-//Jeff imported Update from screens
+//Jeff added history screen
 import History from "./screens/History";
+//Jeff added router/switch
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
-import Edit from "./screens/Edit";  
+//Jeff: added edit screen
+import Edit from "./screens/Edit";
+//Jeff: added Current Balance  
+import CurrentBalance from "./components/CurrentBalance"
+import Header from "./components/Header"
 
 function App() {
   return (
     <Router>
       <div className="App">
+        <Header />
+        {/* Jeff: added <hr/> to help visually seperate components... Make sure it is deleted if needed */}
+        <hr/>
+        <CurrentBalance />
+        {/* Jeff: added <hr/> to help visually seperate components... Make sure it is deleted if needed */}
+        <hr/>
         <Switch>
           <Route path="/" exact component={ Transaction } />
           <Route path="/history"  component={ History } />
---
+++ client-react/src/components/CurrentBalance.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const CurrentBalance = () => {
+    return (
+        <div>
+            <h4>Your Current Balance: </h4>
+        </div>
+    )
+}
+
+export default CurrentBalance; 
