6184a79ed2f42325eb2848332eabe9ef84ee7d1b
Ryan Jacobo <ryanjacobo@yahoo.com>
2020-05-31 21:44:01 -0700

    Transactions POST with JWT work in progress
    Transactions POST with JWT work in progress

--- server-express-mysql/routes/users.js
+++ server-express-mysql/routes/users.js
@@ -34,7 +34,7 @@ router.post('/signup', function (req, res, next) {
   })
 })
 
-// user signup frontend route
+// user login frontend route
 router.get('/login', function (req, res, next) {
   res.render('login');
 });
@@ -60,11 +60,10 @@ router.post('/login', function (req, res, next) {
         // res.send('Login successful');
         console.log('Login successful');
         res.redirect('transactions');
-      }else {
+      } else {
         console.log('Wrong Password');
         res.redirect('login');
       }
-     
     }
   });
 });
@@ -97,20 +96,11 @@ router.get('/transactions', function (req, res, next) {
   }
 })
 
-
-// logout
-router.get('/logout', function (req, res, next) {
-  res.cookie('jwt', '', { expire: new Date(0) });
-  // res.send('Logged out');
-  console.log('Logged out');
-  res.redirect('login')
-});
-
-// create new transaction with user secured route
-router.post('/transactions', function (req, res, next) {
+// POST to transactions without JWT (Working)
+router.post('/:id/transactions', function (req, res, next) {
   models.transactions
     .create({
-      userid: parseInt(req.user.userid),
+      userid: parseInt(req.params.id),
       type: req.body.type,
       amount: req.body.amount,
       description: req.body.description,
@@ -126,9 +116,82 @@ router.post('/transactions', function (req, res, next) {
     })
 })
 
+// create new transaction with user secured route
+// router.post('/transactions', function (req, res, next) {
+//   // let token = req.cookies.jwt;
+//   // if (token) {
+//   //   authService.verifyUser(token)
+//   //   .then(user => {
+//   //     if (user) {
+//   //       res.render('transactions')
+//   //     }  else {
+//   //       res.status(401);
+//   //       res.send('Must be logged in')
+//   //       // console.log('Must be logged in');
+//   //       // res.redirect('login')
+//   //     }
+//   //   })
+//   // }
+//   models.transactions
+//     .create({
+//       userid: parseInt(req.user.userid),
+//       type: req.body.type,
+//       amount: req.body.amount,
+//       description: req.body.description,
+//       date: req.body.date
+//     })
+//     .then(newTransaction => {
+//       // JWT 
+//       let token = authService.signUser(user);
+//       res.cookie('jwt', token);
+//       // res.setHeader('Content-Type', 'application/json');
+//       res.send(JSON.stringify(newTransaction));
+//     })
+//     .catch(err => {
+//       res.status(400);
+//       res.send(err.message);
+//     })
+// })
+
+//findOne user and their transactions with JWT
+// router.get('/:id', function (req, res, next) {
+//   let token = req.cookies.jwt;
+//   if (token) {
+//     authService.verifyUser(token)
+//       .then(user => {
+//         if (user) {
+//           res.render('users', {
+//             include: [{ model: models.transactions }],
+//             where: { userid: parseInt(req.params.id) },
+//             firstname: user.firstname,
+//             lastname: user.lastname
+//           })
+//         } else {
+//           res.status(401);
+//           res.send('Must be logged in')
+//           // console.log('Must be logged in');
+//           // res.redirect('login')
+//         }
+//       })
+//   } else {
+//     res.status(401);
+//     res.send('Must be logged in')
+//     // console.log('Must be logged in');
+//     // res.redirect('login')
+//   }
+// });
+
+// logout
+router.get('/logout', function (req, res, next) {
+  res.cookie('jwt', '', { expire: new Date(0) });
+  // res.send('Logged out');
+  console.log('Logged out');
+  res.redirect('login')
+});
+
 // user profile == for future updates. will contain add'l info i.e. history and stats
 // AJ confirmed working on Postman
-router.get('/profile/', function (req, res, next) {
+router.get('/profile', function (req, res, next) {
   models.users
     .findByPk(parseInt(req.user.userid))
     .then(user => {
@@ -162,7 +225,6 @@ router.put('/', function (req, res, next) {
 //delete with user secured routes
 router.delete('/', function (req, res, next) {
   let userId = parseInt(req.user.userid);
-
   models.users
     .destroy({ where: { userid: userId } })
     .then(result => res.redirect('/'))
@@ -174,6 +236,35 @@ router.delete('/', function (req, res, next) {
 
 
 // =====Admin functions==========
+
+//findAll users
+router.get('/', function (req, res, next) {
+  let token = req.cookies.jwt;
+  authService.verifyUser(token)
+  // models.users
+    .findAll({
+      include: [{ model: models.transactions }]
+        .then(usersFound => {
+          res.setHeader('Content-Type', 'application/json')
+          res.send(JSON.stringify(usersFound));
+        })
+    })
+})
+
+//findOne user and their transactions
+// router.get("/:id", function (req, res, next) {
+//   models.users
+//     .findOne({
+//       include: [{ model: models.transactions }],
+//       where: { userid: parseInt(req.params.id) }
+//     })
+//     .then(usersFound => {
+//       let token = authService.signUser(user);
+//       res.cookie('jwt', token);
+//       res.setHeader('Content-Type', 'application/json');
+//       res.send(JSON.stringify(usersFound));
+//     })
+// });
 
 // Return all app users (findAll)
 // AJ confirmed working on Postman
@@ -191,20 +282,6 @@ router.delete('/', function (req, res, next) {
 //       res.send(JSON.stringify(usersFound))
 //     })
 // })
-
-//findOne user and their transactions
-// router.get("/:id", function (req, res, next) {
-//   models.users
-//     .findOne({
-//       // causes an error that transactions is not associated with users
-//       include: [{ model: models.transactions }],
-//       where: { userid: parseInt(req.params.id) }
-//     })
-//     .then(usersFound => {
-//       res.setHeader('Content-Type', 'application/json');
-//       res.send(JSON.stringify(usersFound));
-//     })
-// });
 
 // Test users/:id route. Not needed.
 // router.get('/:id', function (req, res, next) {
