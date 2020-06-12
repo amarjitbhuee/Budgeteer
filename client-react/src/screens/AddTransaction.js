13e9b9af7b144b51789ca14fcb9b95b89ffd78fb
Jeff C <coen.jeffery@outlook.com>
2020-06-11 18:23:28 -0700

    add alerts on update and delete

--- client-react/src/screens/AddTransaction.js
+++ client-react/src/screens/AddTransaction.js
@@ -73,9 +73,8 @@ class AddTransaction extends React.Component {
           <table className="table">
             <tbody>
               <tr>
-                <td><select required ref={this.paymentType} className="selectFields">
-                  <option value="N/A" className="oops">Payment Type: </option>
-                  <option value="" disabled className="type">Select Payment Type: </option>
+                <td><select required ref={this.paymentType} className="selectFields" defaultValue="N/A" name="paymentType">
+                  <option value="N/A" disabled className="type">Select Payment Type: </option>
                   <option value="Direct Deposit">Direct Deposit</option>
                   <option value="Check">Check</option>
                   <option value="Credit Card">Credit Card</option>
@@ -83,16 +82,15 @@ class AddTransaction extends React.Component {
                   <option value="Other">Other</option>
                 </select></td>
                 <td><DatePicker selected={this.state.date} onChange={this.handleChange} placeholderText="Date" /></td>
-                <td><select ref={this.type} className="selectFields">
-                  <option value="N/A (Was not Added to Your Balances)" className="Oops">Type:</option>
-                  <option defaultValue="" disabled="disabled" className="type">Transaction Type: </option>
+                <td><select ref={this.type} className="selectFields" defaultValue="N/A (Amount Was Not Added)" name="type">
+                  <option value="N/A (Amount Was Not Added)" disabled className="type">Transaction Type: </option>
                   <option value="Income">Income</option>
                   <option value="Expense">Expense</option>
                   <option value="Savings">Savings</option>
                 </select></td>
                 <td>
                 <input ref={this.amount} className="amount" placeholder="$ Dollar Amount" type="number"  /></td>
-                <td><input ref={this.description} className="description" placeholder="Description" /></td>
+                <td><input ref={this.description} className="description" placeholder="Description"/></td>
               </tr>
             </tbody>
           </table>
--- client-react/src/screens/Edit.js
+++ client-react/src/screens/Edit.js
@@ -47,21 +47,12 @@ class Edit extends React.Component {
       description: this.description.current.value
     })
       .then(response => {
+        alert('Hey there Budgeteer!\n\nYou Applied An Update To Your Transaction!')
         window.location.reload();
         // refresh the data
         this.getTransaction();
       })
       .catch(err => console.log(err));
-  };
-
-  deleteTransaction = (transactionid) => {
-    let url = "http://localhost:3001/transactions/" + transactionid;
-    axios.delete(url)
-      .then(response => {
-        this.getData();
-        alert('Your Transaction has been deleted!');
-        window.location.reload();
-      })
   };
 
   render() {
@@ -71,52 +62,54 @@ class Edit extends React.Component {
         <form className="form">
           <table className="table">
             <thead>
-            <tr>
+            <tr className="editFields">
               <th></th>
-              <th>Edit Payment Type:</th>
-              <th>Edit Date:</th>
-              <th>Edit Payment Type:</th>
-              <th>Edit Amount:</th>
-              <th>Edit Description:</th>
+              <th className="editTitle">Edit Payment Type:</th>
+              <th className="editTitle">Edit Date:</th>
+              <th className="editTitle">Edit Payment Type:</th>
+              <th className="editTitle">Edit Amount:</th>
+              <th className="editTitle">Edit Description:</th>
               <th></th>
             </tr>
             </thead>
             <tbody>
             <tr>
-              <td>Entered</td>
-              <td>{this.state.transactions.paymentType}</td>
-              <td>{this.state.transactions.date} </td>
-              <td>{this.state.transactions.type}</td>
-              <td>${this.state.transactions.amount}</td>
-              <td>{this.state.transactions.description}</td>
+              <td>Entered: </td>
+              <td className="editField">{this.state.transactions.paymentType}</td>
+              <td className="editField">{this.state.transactions.date} </td>
+              <td className="editField">{this.state.transactions.type}</td>
+              <td className="editField">${this.state.transactions.amount}</td>
+              <td className="editField">{this.state.transactions.description}</td>
             </tr>
             </tbody>
             <tbody>
             <tr>
-              <td>Edit</td>
-              <td><select ref={this.paymentType} className="selectFields2">
-                  <option value="N/A (Did not affect balances)" className="oops">Payment Type: </option>
-                  <option value=""  disabled className="type">Select Payment Type: </option>
-                  <option value="Direct Deposit">Direct Deposit</option>
-                  <option value="Check">Check</option>
-                  <option value="Credit Card">Credit Card</option>
-                  <option value="Cash">Cash</option>
-                  <option value="Other">Other</option>
+              <td>Edit: </td>
+              <td><select ref={this.paymentType} className="selectFields2" name="paymentType">
+                  <option value={this.state.transactions.paymentType}>{this.state.transactions.paymentType}</option>
+                  <optgroup label="Select Payment Type:">
+                    <option value="Direct Deposit">Direct Deposit</option>
+                    <option value="Check">Check</option>
+                    <option value="Credit Card">Credit Card</option>
+                    <option value="Cash">Cash</option>
+                    <option value="Other">Other</option>
+                  </optgroup>
               </select></td>
               <td><DatePicker selected={this.state.date} onChange={this.handleChange} placeholderText={this.state.transactions.date} /></td>
-              <td><select ref={this.type} className="selectFields2">
-                  <option value="N/A (Was not Added to Your Balances)" className="oops">Type:</option>
-                  <option value="" disabled className="type">Transaction Type:</option>
-                  <option value="Income">Income</option>
-                  <option value="Expense">Expense</option>
-                  <option value="Savings">Savings</option>
+              <td><select ref={this.type} className="selectFields2" defaultValue={this.state.transactions.Type} name="type">
+                  <option value={this.state.transactions.type}>{this.state.transactions.type}</option>
+                  <optgroup label="Transaction Type: ">
+                    <option value="Income">Income</option>
+                    <option value="Expense">Expense</option>
+                    <option value="Savings">Savings</option>
+                  </optgroup>
               </select></td>
               <td><input ref={this.amount} className="amount" defaultValue={this.state.transactions.amount} /></td>
               <td><input ref={this.description} className="description" defaultValue={this.state.transactions.description} required /></td>
-              <td><button type="button" className="btn btn-success" onClick={() => this.updateTransaction(this.transactionid)}>Update</button></td>
             </tr>
             </tbody>
           </table>
+          <button type="button" className="update" onClick={() => this.updateTransaction(this.transactionid)}>Update</button>
         </form>
         <p className="note">*** Please make ensure that all fields are filled out prior to update ***<br />- Team Penguin</p>
         <Links />
